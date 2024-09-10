44bb7acd2a1c76bdaf76ee5aa9829023997df5ba
Pola-Gur <pola.gur.il@gmail.com>
2024-09-10 15:58:29 +0300

    separate Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home'; // Предположим, что у тебя есть Home компонент
-import About from './components/About';
-import Contacts from './components/Contacts';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About'; // Создай компонент About
+import Contacts from './components/Contacts'; // Создай компонент Contacts
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'; // Убедись, что путь к CSS-файлу правильный
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,40 @@
+// src/components/Navbar.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container px-4 px-lg-5">
+                <NavLink className="navbar-brand" to="/">Shop name</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/" end>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <button className="btn btn-outline-dark" type="submit">
+                            <i className="bi-cart-fill me-1"></i>
+                            Cart
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
